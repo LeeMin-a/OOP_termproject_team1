12fd86c87072b46461986358e7e7aec78c0319de
LeeMin-a <59161563+LeeMin-a@users.noreply.github.com>
2021-06-15 22:44:53 +0900

    Update Appointment.java

--- project/Appointment.java
+++ project/Appointment.java
@@ -1,14 +1,23 @@
 package project;
 
+/**
+ * Appointment class that inherit Plan class
+ * Author : Lee Min A
+ * Last modify : 2021 / 05 / 29
+ * Term project of OOP_Team 1
+ */
+
 public class Appointment extends Plan
 {
 	private String friendName;
 	
+	// get a name of content's person
 	public String getFriendName() 
 	{
 		return friendName;
 	}
 	
+	// set a name of content's person
 	public void setFriendName(String friendName) 
 	{
 		this.friendName = friendName;
