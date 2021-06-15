c6daaf7b73f5246789719490a7e1c5cfc83b7eb3
LeeMin-a <59161563+LeeMin-a@users.noreply.github.com>
2021-06-15 22:59:02 +0900

    Update Goal.java

--- project/Goal.java
+++ project/Goal.java
@@ -1,14 +1,23 @@
 package project;
 
+/**
+ * Goal class that inherit Plan class
+ * Author : Lee Min A
+ * Last modify : 2021 / 05 / 29
+ * Term project of OOP_Team 1
+ */
+
 public class Goal extends Plan
 {
 	private String importance;
 		
+	// get a content's importance
 	public String getImportance() 
 	{
 		return importance;
 	}
-		
+	
+	// set a content's importance
 	public void setImportance(String importance) 
 	{
 		this.importance = importance;
