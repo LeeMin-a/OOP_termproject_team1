3f3d40d81f238bacbaaea692757c3af1afafdb24
LeeMin-a <59161563+LeeMin-a@users.noreply.github.com>
2021-06-15 22:58:14 +0900

    Update Assignment.java

--- project/Assignment.java
+++ project/Assignment.java
@@ -1,15 +1,23 @@
 package project;
 
+/**
+ * Assignment class that inherit Plan class
+ * Author : Lee Min A
+ * Last modify : 2021 / 05 / 29
+ * Term project of OOP_Team 1
+ */
+
 public class Assignment extends Plan
 {
 	private String difficulty;
 	
+	// get a content's difficulty
 	public String getDifficulty() 
 	{
 		return difficulty;
 	}
 	
-	
+	// set a content's difficulty
 	public void setDifficulty(String difficulty) 
 	{
 		this.difficulty = difficulty;
