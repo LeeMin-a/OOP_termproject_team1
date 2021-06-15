32e91f59835e472f7935a6a5c52890c536440678
서은수 <83586230+eunsu0613@users.noreply.github.com>
2021-06-15 18:29:29 +0900

    Add files via upload

---
+++ project/DateInfoInterface.java
@@ -0,0 +1,17 @@
+package project;
+/**
+ * Implements PlanInterface.
+ * @author Seo Eun Su.
+ * last modified 21-06-15.
+ */
+public interface DateInfoInterface {
+	public String getMonth();
+	public void setMonth(String month);
+	public String getDay();
+	public void setDay(String day);
+	public String getDayOfTheWeek();
+	public void setDayOfTheWeek(String dayOfTheWeek);
+	public String getTime();
+	public void setTime(String time);
+	public String getInfo();
+}
